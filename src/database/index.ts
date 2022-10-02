9418c6c13b7995e52cf7f66f30326adf8d96148f
matheusinit <matheus.oliveira.s@protonmail.com>
2022-10-02 12:02:45 -0300

    add environment variable PG_PORT

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,7 +7,7 @@ dotenv.config()
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.PG_HOST,
-  port: 5432,
+  port: Number(process.env.PG_PORT) ?? 5432,
   username: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DB,
